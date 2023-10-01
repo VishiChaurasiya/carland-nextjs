286fe36cc624cebf2a0b5a27a7ddaa340e7e538a
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2023-10-02 04:16:27 +0530

    Add comment in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   productionBrowserSourceMaps: true, // Disable source maps in development
   optimizeFonts: false, // Disable font optimization
   minify: false, // Disable minification
+
+  // comment the below code and also add '/' in all images src when running the app in local mode
   output: "export",
   basePath: "/carland-nextjs",
 };
