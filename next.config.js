9130a9a3bbe6f3055047d989413d630109dfb3d1
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2023-10-01 23:35:33 +0530

    CTA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  fastRefresh: true,
+  productionBrowserSourceMaps: true, // Disable source maps in development
+  optimizeFonts: false, // Disable font optimization
+  minify: false, // Disable minification
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
